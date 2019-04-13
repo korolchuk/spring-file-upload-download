c88488d2049366efae6c4c90ffa9165e0d505c9f
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2019-04-14 00:19:45 +0200

    Initial commit

---
+++ src/main/java/fr/ukaralchuk/springfileuploaddownload/SpringFileUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package fr.ukaralchuk.springfileuploaddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileUploadDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFileUploadDownloadApplication.class, args);
+    }
+
+}
